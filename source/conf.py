6a8c06f5e9a2ad6f9823f5324092cea5378fb297
Hanfu Zhao <hanfuzhao@Hanfus-MacBook-Pro.local>
2019-11-09 18:20:33 +0800

    set master_doc in config

--- source/conf.py
+++ source/conf.py
@@ -63,3 +63,8 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+
+# for readthedocs web hosting
+master_doc = 'index'
