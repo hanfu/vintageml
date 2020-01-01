7a8be86b0cf5ce5b3ff06ec4949987c2769adbf3
Hanfu Zhao <hanfuzhao@Hanfus-MacBook-Pro.local>
2020-01-01 20:26:06 +0800

    update for sitemap

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,7 @@ release = '0.01'
 extensions = [
 'sphinx.ext.mathjax',
 'sphinx.ext.autosectionlabel',
-'sphinx_sitemap',
+# 'sphinx_sitemap',
 ]
 
 # autosectionlabel settings
@@ -52,7 +52,7 @@ html_extra_path = ['robots.txt', 'sitemap.xml']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+# language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
