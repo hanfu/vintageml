bc02038786b954ee2022983429533b6f5743be0b
Hanfu Zhao <hanfuzhao@Hanfus-MacBook-Pro.local>
2020-01-01 20:48:00 +0800

    added 3rd-party generated sitemap

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,7 @@ html_extra_path = ['robots.txt', 'sitemap.xml']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-# language = None
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
